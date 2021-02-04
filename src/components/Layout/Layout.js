ed7d7efdb6cc767279d53640eb563a7f45217063
Adam Gajzlerowicz <nelf86@gmail.com>
2021-02-04 23:54:08 +0100

    Add gatsby, update fonts

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -21,19 +21,6 @@ import GlobalStyle from "../../utils/globalStyle";
 
 import imgFavicon from "../../assets/favicon.ico";
 
-import "../../assets/fonts/fontawesome-5/webfonts/fa-brands-400.ttf";
-import "../../assets/fonts/fontawesome-5/webfonts/fa-regular-400.ttf";
-import "../../assets/fonts/fontawesome-5/webfonts/fa-solid-900.ttf";
-import "../../assets/fonts/typography-font/Circular-Std-Book.ttf";
-import "../../assets/fonts/typography-font/CircularStd-Black.ttf";
-import "../../assets/fonts/typography-font/CircularStd-BlackItalic.ttf";
-import "../../assets/fonts/typography-font/CircularStd-Bold.ttf";
-import "../../assets/fonts/typography-font/CircularStd-BoldItalic.ttf";
-import "../../assets/fonts/typography-font/CircularStd-Book.ttf";
-import "../../assets/fonts/typography-font/CircularStd-BookItalic.ttf";
-import "../../assets/fonts/typography-font/CircularStd-Medium.ttf";
-import "../../assets/fonts/typography-font/CircularStd-MediumItalic.ttf";
-
 import "../../assets/fonts/icon-font/fonts/avasta.ttf";
 import "../../assets/fonts/icon-font/css/style.css";
 
@@ -43,7 +30,6 @@ import "../../../node_modules/slick-carousel/slick/slick-theme.css";
 import "../../../node_modules/aos/dist/aos.css";
 
 import "../../assets/fonts/icon-font/css/style.css";
-import "../../assets/fonts/typography-font/typo.css";
 import "../../assets/fonts/fontawesome-5/css/all.css";
 
 import { get, merge } from "lodash";
