0bfca233b64336daecc515dc2a61a927f760fcfa
Adam Gajzlerowicz <nelf86@gmail.com>
2021-02-05 00:16:26 +0100

    Add gatsby, update fonts

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -2,7 +2,7 @@ import { rgba } from "polished";
 import { breakpoints } from "./breakpoints";
 
 const defaultColors = {
-  primary: "#f04037",
+  primary: "#334D8A",
   secondary: "#5454d4",
   white: "#ffffff",
   dark: "#19191b",
