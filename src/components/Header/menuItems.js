9bbd403844143ec5cebd628b9e090127c162f50b
Adam Gajzlerowicz <nelf86@gmail.com>
2021-02-09 17:51:36 +0100

    Added shoots

--- src/components/Header/menuItems.js
+++ src/components/Header/menuItems.js
@@ -1,15 +1,11 @@
 export const menuItems = [
   {
-    name: "aktualnosci",
-    label: "Aktualności",
-  },
-  {
     name: "nasza-misja",
     label: "Nasza misja",
   },
   {
-    name: "polityka-prywatnosci",
-    label: "Polityka prywatności"
+    name: "aktualnosci",
+    label: "Aktualności",
   },
   {
     name: "kontakt",
--- src/sections/about/Content.js
+++ src/sections/about/Content.js
@@ -25,24 +25,27 @@ const Content = () => (
           </Col>
           <Col lg="6" className="pl-lg-5">
             <Text>
-                Doskonale wiem jak trudno jest znaleźć osoby, od której można nauczyć się poprawnej opieki nad psem.
-                Po tym jak Mongo opuścił schronisko i przyłączył się do naszej rodziny, otrzymałem kontakt do behawiorysty,
-                który dał nam pierwsze wskazówki dotyczące zajmowania się nim. Kiedy współpraca zakończyła się, było mi bardzo trudno
-                trudno znaleźć miejsce gdzie moglibyśmy kontynuować naukę lub uczyć sie nowych rzeczy.
+                Dnia 6 marca 2017 roku adoptowałem Mongo - półrocznego psiaka ze schroniska.
+                Od razu otrzymałem kontakt do behawiorysty, dzięki któremu nauczyliśmy się podstawowych komend i posłuszeństwa.
+                Po zakończeniu psiego przedszkola chciałem jednak dalej pracować z psem -
+                Mongo się świetnie bawił, a ja zdałem sobie sprawę,
+                że moja aktywność z psem nie musi się ograniczać do wyjścia na spacer.
+                Znalezienie trenera okazało się czymś absurdalnie trudnym - nie wiedziałem, gdzie moglibyśmy
+                kontynuować naukę lub spędzać aktywnie czas.
             </Text>
 
             <Text>
-                Pomimo tego, że na przełomie ostatnich lat spotkaliśmy z Mongo wiele fantastycznych osób, którym wiele zadzięczam i
-                które wiele nas nauczyły, nadal sprawia wyzwaanie umówienie sie na zajęcia
-                do osoby u której ćwiczyłem wcześniej, nie wspominając o tym ile jest osób u których mógłbym ćwiczyć,
-                a ktorych jeszcze nie poznałem.
+                Nawet kiedy udało mi się zdobyć kontakt do jakiegoś trenera, to często okazywało się, że nie jesteśmy na
+                odpowiednim poziomie nauki albo po prostu nie ma dostępnych terminów.
+                Nasza misja polega na zwiększeniu przezroczystości rynku związanego z psimi zaktywnościami. Tak właśnie powstał Agilix.
+                Aplikacja na telefon, dzięki której można znaleźć trenerów i nowe typy zajęć.
+                Nie ważne, czy ma się psa przez całe swoje życie, czy tylko od wczoraj.
+                Każdy powinien mieć szansę na spędzanie czasu ze swoim pupilem.
             </Text>
 
               <Text>
-                  Nasza misja polega na zwiększeniu przezroczystości rynku związanego z psimi zaktywnościami. Tak właśnie powstał Agilix.
-                  Aplikacja na telefon, dzięki której można znaleźć trenerów i nowe typy zajęć.
-                  Nie ważne, czy ma się psa przez całe swoje życie, czy tylko od wczoraj.
-                  Każdy powinien mieć szansę na spędzanie czasu ze swoim pupilem.
+                  Agilix ma zwiększyć przezroczystość rynku związanego z psimi aktywnościami: każdy może umówić się na wybrane
+                  zajęcia a trenerzy są w stanie zarządzać swoimi wydarzeniami.
               </Text>
           </Col>
 
