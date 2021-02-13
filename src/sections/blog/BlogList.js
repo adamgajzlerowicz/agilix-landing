7f4f99360a2dcda61527dc4a9f9e083cb83c55e8
Adam Gajzlerowicz <nelf86@gmail.com>
2021-02-13 16:56:50 +0100

    added contact form

--- src/sections/blog/BlogList.js
+++ src/sections/blog/BlogList.js
@@ -1,44 +1,56 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import {Container, Row, Col} from "react-bootstrap";
 
-import { Section } from "../../components/Core";
+import {Section} from "../../components/Core";
 import PostCard from "../../components/PostCard";
 
 const BlogList = () => (
-  <>
-    <Section className="position-relative">
-      <Container>
-        <Row className="align-items-center justify-content-center">
-          <Col lg="4" className="mb-5">
-            <PostCard
-              preTitle="6 Luty 2021"
-              title="Powstała strona internetowa."
-            >
-                Pierwszy projekt strony internetowej jest już dostępny.
-                <br />
-              Będą tutaj sie powiawiać aktualności zwiazane z życiem aplikacji oraz nowo stworzone funkcjonalności.
-            </PostCard>
-          </Col>
+    <>
+        <Section className="position-relative">
+            <Container>
+                <Row className="align-items-center justify-content-center">
+                    <Col lg="4" className="mb-5">
+                        <PostCard
+                            preTitle="13 Luty 2021"
+                            title="Formularz kontaktowy został ukończony"
+                        >
+                            Z upływem czasu strona jest rozwijana i dodawane są nowe funkcjojnalności.
+                            <br/>
+                            Od dziś, formularz kontaktowy jest sprawny.
+                        </PostCard>
+                    </Col>
 
-        </Row>
-        {/*<Box className="d-flex justify-content-center" mt={4}>*/}
-        {/*  <Pagination>*/}
-        {/*    <PageItem>*/}
-        {/*      <FaAngleLeft />*/}
-        {/*    </PageItem>*/}
-        {/*    <PageItem>1</PageItem>*/}
-        {/*    <PageItem>2</PageItem>*/}
-        {/*    <PageItem>3</PageItem>*/}
-        {/*    <PageItem>...</PageItem>*/}
-        {/*    <PageItem>9</PageItem>*/}
-        {/*    <PageItem>*/}
-        {/*      <FaAngleRight />*/}
-        {/*    </PageItem>*/}
-        {/*  </Pagination>*/}
-        {/*</Box>*/}
-      </Container>
-    </Section>
-  </>
+                    <Col lg="4" className="mb-5">
+                        <PostCard
+                            preTitle="6 Luty 2021"
+                            title="Powstała strona internetowa."
+                        >
+                            Pierwszy projekt strony internetowej jest już dostępny.
+                            <br/>
+                            Będą tutaj sie powiawiać aktualności zwiazane z życiem aplikacji oraz nowo stworzone
+                            funkcjonalności.
+                        </PostCard>
+                    </Col>
+
+                </Row>
+                {/*<Box className="d-flex justify-content-center" mt={4}>*/}
+                {/*  <Pagination>*/}
+                {/*    <PageItem>*/}
+                {/*      <FaAngleLeft />*/}
+                {/*    </PageItem>*/}
+                {/*    <PageItem>1</PageItem>*/}
+                {/*    <PageItem>2</PageItem>*/}
+                {/*    <PageItem>3</PageItem>*/}
+                {/*    <PageItem>...</PageItem>*/}
+                {/*    <PageItem>9</PageItem>*/}
+                {/*    <PageItem>*/}
+                {/*      <FaAngleRight />*/}
+                {/*    </PageItem>*/}
+                {/*  </Pagination>*/}
+                {/*</Box>*/}
+            </Container>
+        </Section>
+    </>
 );
 
 export default BlogList;
