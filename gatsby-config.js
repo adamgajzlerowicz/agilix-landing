c37e9558232eac35ca02ec7d89cabe79c61fc360
adamgajzlerowicz <nelf86@gmail.com>
2021-05-12 09:35:57 +0200

    https fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteMetadata: {
     title: `Agilix`,
     description: 'Baza dla wszsystkich miłośników aktywnego spędzania czasu z psami',
-    author: `Adam Gajzlerowwicz`,
+    author: `Adam Gajzlerowicz`,
     siteUrl: `https://agilix.dog`
   },
   plugins: [
@@ -18,17 +18,17 @@ module.exports = {
       options: {
         trackingId: "UA-196721675-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        // head: false,
         // Setting this parameter is optional
-        anonymize: true,
+        // anonymize: true,
         // Setting this parameter is also optional
-        respectDNT: true,
+        // respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
         // Delays sending pageview hits on route update (in milliseconds)
         // pageTransitionDelay: 0,
         // Defers execution of google analytics script after page load
-        defer: false,
+        // defer: false,
         // Any additional optional fields
         // sampleRate: 5,
         // siteSpeedSampleRate: 10,
