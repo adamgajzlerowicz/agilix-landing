f1fbfc16199f34f187cf3d6d141f09d55570fbcb
Adam Gajzlerowicz <nelf86@gmail.com>
2022-05-24 23:46:21 +0200

    Upgrade gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "G-0003FLN5B7",
+          "GA-3585733083",
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
