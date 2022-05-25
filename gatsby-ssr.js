b7684eb9d386baf1346389d3019e36762fe154b5
Adam Gajzlerowicz <nelf86@gmail.com>
2022-05-25 10:56:45 +0200

    Upgrade gatsby

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import { GlobalProvider } from './src/context/GlobalContext';
-import Layout from './src/components/Layout';
+import React from 'react'
+import { GlobalProvider } from './src/context/GlobalContext'
+import Layout from './src/components/Layout'
 
 export const wrapPageElement = ({ element, props }) => (
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
   <Layout {...props}>{element}</Layout>
-);
+)
 
-export const wrapRootElement = ({ element }) => (
-  <GlobalProvider>{element}</GlobalProvider>
-);
+export const wrapRootElement = ({ element }) => <GlobalProvider>{element}</GlobalProvider>
