f9526402880417517918244947e27e28e31b788a
Adam Gajzlerowicz <nelf86@gmail.com>
2022-05-25 11:28:56 +0200

    Upgrade gatsby

--- src/components/seo.js
+++ src/components/seo.js
@@ -4,22 +4,16 @@ import { Helmet } from 'react-helmet'
 import { useLocation } from '@reach/router'
 import { useStaticQuery, graphql } from 'gatsby'
 
-export const Seo = ({ title, description, image, article }) => {
+export const Seo = ({ title }) => {
   const { pathname } = useLocation()
   const { site } = useStaticQuery(query)
 
-  const {
-    defaultTitle,
-    titleTemplate,
-    defaultDescription,
-    siteUrl,
-    defaultImage,
-    twitterUsername,
-  } = site.siteMetadata
+  const { title: defaultTitle, titleTemplate, description, siteUrl, image } = site.siteMetadata
+
   const seo = {
     title: title || defaultTitle,
-    description: description || defaultDescription,
-    image: `${siteUrl}${image || defaultImage}`,
+    description: description,
+    image: `${siteUrl}${image}`,
     url: `${siteUrl}${pathname}`,
   }
 
@@ -28,12 +22,11 @@ export const Seo = ({ title, description, image, article }) => {
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
       {seo.url && <meta property="og:url" content={seo.url} />}
-      {(article ? true : null) && <meta property="og:type" content="article" />}
+      {/*{(article ? true : null) && <meta property="og:type" content="article" />}*/}
       {seo.title && <meta property="og:title" content={seo.title} />}
       {seo.description && <meta property="og:description" content={seo.description} />}
       {seo.image && <meta property="og:image" content={seo.image} />}
       <meta name="twitter:card" content="summary_large_image" />
-      {twitterUsername && <meta name="twitter:creator" content={twitterUsername} />}
       {seo.title && <meta name="twitter:title" content={seo.title} />}
       {seo.description && <meta name="twitter:description" content={seo.description} />}
       {seo.image && <meta name="twitter:image" content={seo.image} />}
